8ce03e4c47c7a7574fc100dd5f5ed270595ece15
faisalhatila <hatilas1994@gmail.com>
2025-01-10 08:10:41 +0500

    animation added to popup

--- src/components/MessageRoom.tsx
+++ src/components/MessageRoom.tsx
@@ -3,6 +3,7 @@ import ChatMessage from './ChatMessage';
 import { MessageType } from './Chatbox';
 import { useAuth } from '@/app/context/AuthContext';
 import { IoMdCloseCircleOutline } from 'react-icons/io';
+import { motion } from 'framer-motion';
 
 interface MessageRoomProps {
   messages: MessageType[];
@@ -10,7 +11,7 @@ interface MessageRoomProps {
 }
 const MessageRoom = ({ messages, setIsLoading }: MessageRoomProps) => {
   const messageRoomRef: any = useRef(null);
-  const [isDisplayPopup, setIsDisplayPopup] = useState(true);
+  const [isDisplayPopup, setIsDisplayPopup] = useState(false);
   useEffect(() => {
     if (messageRoomRef.current) {
       messageRoomRef.current?.scrollTo({
@@ -20,14 +21,29 @@ const MessageRoom = ({ messages, setIsLoading }: MessageRoomProps) => {
     }
   }, [messages]);
   const { attemptsLeft } = useAuth();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsDisplayPopup(true);
+    }, 800);
+
+    // Clean up the timeout when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleClosePopup = () => {
     setIsDisplayPopup(false);
   };
 
+
   return (
     <div className="flex-1 px-2 pt-3">
       {isDisplayPopup && (
+        <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -50 }}
+        transition={{ duration: 0.5 }}
+      >
         <div className="flex justify-center">
           <div className="fixed z-10 bg-[#ffff90] text-black text-[14px] px-2 py-1 flex items-center gap-[5px] bg-opacity-95">
             ({attemptsLeft}) in header shows attempts left for today
@@ -37,7 +53,9 @@ const MessageRoom = ({ messages, setIsLoading }: MessageRoomProps) => {
             />
           </div>
         </div>
+      </motion.div>
       )}
+
       <div
         ref={messageRoomRef}
         className="h-[460px] overflow-auto flex flex-col gap-[10px]"
