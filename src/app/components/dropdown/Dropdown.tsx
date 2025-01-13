2973097ef18d6997d58d40acc1141ed4833b5e7c
faisalhatila <hatilas1994@gmail.com>
2025-01-13 07:10:38 +0500

    logout functionality aded

--- src/app/components/dropdown/Dropdown.tsx
+++ src/app/components/dropdown/Dropdown.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import { useAuth } from '@/app/context/AuthContext';
+import { RiLogoutCircleLine } from "react-icons/ri";
+import { FaHistory } from "react-icons/fa";
+import { AiOutlineWechat } from "react-icons/ai";
+
+
+
 
 const Dropdown = () => {
+
+    const {logout} = useAuth()
   return (
     <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md overflow-hidden">
       <button
-        className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
-        onClick={() => alert('Logout')}
+        className="flex items-center gap-[5px] w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
+        onClick={logout}
       >
-        Logout
+        <RiLogoutCircleLine /> Logout
       </button>
       <button
-        className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
+        className="flex items-center gap-[5px] w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
         onClick={() => alert('Chat History')}
       >
-        Chat History
+        <FaHistory /> Chat History
       </button>
       <button
-        className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
+        className="flex items-center gap-[5px] w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200"
         onClick={() => alert('New Chat')}
       >
-        New Chat
+        <AiOutlineWechat /> New Chat
       </button>
     </div>
   );
